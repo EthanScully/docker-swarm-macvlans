264ed73b23d47c14d9e525595512d89103c22ea3
EthanScully <89865501+EthanScully@users.noreply.github.com>
2024-05-27 16:13:31 -0400

    removed panic out during service, prettier print-out

--- main.go
+++ main.go
@@ -14,12 +14,6 @@ import (
 )
 
 func main() {
-	go loop()
-	inter := make(chan os.Signal, 1)
-	signal.Notify(inter, os.Interrupt, syscall.SIGTERM)
-	<-inter
-}
-func loop() {
 	config := "config.yaml"
 	if len(os.Args) > 1 {
 		config = os.Args[1]
@@ -34,66 +28,87 @@ func loop() {
 		panic(err)
 	}
 	services := data.([]any)
-	for {
-		for _, service := range services {
-			network := service.(map[string]any)["net"].(string)
-			name := service.(map[string]any)["name"].(string)
-			ip := service.(map[string]any)["ip"].(string)
-			cmd := exec.Command("docker", "container", "ls", "-q", "-f", fmt.Sprintf("name=%s", name))
-			fmt.Println(cmd.Args)
-			output, err := cmd.CombinedOutput()
+	go func() {
+		for {
+			update(services)
+			time.Sleep(time.Second * 15)
+		}
+	}()
+	inter := make(chan os.Signal, 1)
+	signal.Notify(inter, os.Interrupt, syscall.SIGTERM)
+	<-inter
+}
+func update(services []any) {
+	go func() {
+		r := recover()
+		if r != nil {
+			fmt.Printf("panic: %v\n", r)
+		}
+	}()
+	for _, service := range services {
+		network := service.(map[string]any)["net"].(string)
+		name := service.(map[string]any)["name"].(string)
+		ip := service.(map[string]any)["ip"].(string)
+		cmd := exec.Command("docker", "container", "ls", "-q", "-f", fmt.Sprintf("name=%s", name))
+		cmdPrint(cmd.Args)
+		output, err := cmd.CombinedOutput()
+		fmt.Println(string(output))
+		if err != nil {
+			panic(err)
+		}
+		id := string(output)
+		cmd = exec.Command("docker", "network", "inspect", network)
+		cmdPrint(cmd.Args)
+		output, err = cmd.CombinedOutput()
+		if err != nil {
+			panic(err)
+		}
+		var data any
+		err = json.Unmarshal(output, &data)
+		if err != nil {
+			panic(err)
+		}
+		l := 12
+		id = id[:l]
+		containers := data.([]any)[0].(map[string]any)["Containers"].(map[string]any)
+		var found bool
+		for k, v := range containers {
+			if k[:l] == id {
+				found = true
+				currentIP := v.(map[string]any)["IPv4Address"].(string)
+				if !strings.Contains(currentIP, ip) {
+					cmd = exec.Command("docker", "network", "disconnect", network, id)
+					cmdPrint(cmd.Args)
+					output, err = cmd.CombinedOutput()
+					fmt.Println(string(output))
+					if err != nil {
+						panic(err)
+					}
+					cmd = exec.Command("docker", "network", "connect", "--ip", ip, network, id)
+					cmdPrint(cmd.Args)
+					output, err = cmd.CombinedOutput()
+					fmt.Println(string(output))
+					if err != nil {
+						panic(err)
+					}
+				}
+			}
+		}
+		if !found {
+			cmd = exec.Command("docker", "network", "connect", "--ip", ip, network, id)
+			cmdPrint(cmd.Args)
+			output, err = cmd.CombinedOutput()
 			fmt.Println(string(output))
 			if err != nil {
 				panic(err)
 			}
-			id := string(output)
-			cmd = exec.Command("docker", "network", "inspect", network)
-			fmt.Println(cmd.Args)
-			output, err = cmd.CombinedOutput()
-			if err != nil {
-				panic(err)
-			}
-			var data any
-			err = json.Unmarshal(output, &data)
-			if err != nil {
-				panic(err)
-			}
-			l := 12
-			id = id[:l]
-			containers := data.([]any)[0].(map[string]any)["Containers"].(map[string]any)
-			var found bool
-			for k, v := range containers {
-				if k[:l] == id {
-					found = true
-					currentIP := v.(map[string]any)["IPv4Address"].(string)
-					if !strings.Contains(currentIP, ip) {
-						cmd = exec.Command("docker", "network", "disconnect", network, id)
-						fmt.Println(cmd.Args)
-						output, err = cmd.CombinedOutput()
-						fmt.Println(string(output))
-						if err != nil {
-							panic(err)
-						}
-						cmd = exec.Command("docker", "network", "connect", "--ip", ip, network, id)
-						fmt.Println(cmd.Args)
-						output, err = cmd.CombinedOutput()
-						fmt.Println(string(output))
-						if err != nil {
-							panic(err)
-						}
-					}
-				}
-			}
-			if !found {
-				cmd = exec.Command("docker", "network", "connect", "--ip", ip, network, id)
-				fmt.Println(cmd.Args)
-				output, err = cmd.CombinedOutput()
-				fmt.Println(string(output))
-				if err != nil {
-					panic(err)
-				}
-			}
 		}
-		time.Sleep(time.Second * 15)
 	}
+}
+func cmdPrint(cmd []string) {
+	list := make([]any, len(cmd))
+	for i, c := range cmd {
+		list[i] = c
+	}
+	fmt.Println(list...)
 }
